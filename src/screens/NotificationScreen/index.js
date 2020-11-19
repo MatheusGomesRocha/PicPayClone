ad1fa4a86f0d8f8e27973a78f6d4edea569d0fb6
Matheus Gomes <matheusgomes192@hotmail.com>
2020-11-19 11:06:37 -0300

    UPDATE, criando e finalizando NotificationScreen

--- src/screens/NotificationScreen/index.js
+++ src/screens/NotificationScreen/index.js
@@ -1,14 +1,57 @@
 import React from 'react';
 
+import { StatusBar } from 'react-native';
+
 import {
     Container,
-    Texto
+
+    Flat,
+
+    Header,
+    BtnConfig,
+
+    ArrayItem,
+    DefaultText,
 } from './styled';
 
+let array = [
+    { id: '1', content: 'Quer ganhar prêmios em dinheiro? Com Unilever + PicPay dá! Vem saber mais.', date: 'Ontem às 19:05' },
+    { id: '2', content: 'Quer ganhar prêmios em dinheiro? Com Unilever + PicPay dá! Vem saber mais. Quer ganhar prêmios em dinheiro? Com Unilever + PicPay dá! Vem saber mais.', date: 'Ontem às 19:05' },
+    { id: '3', content: 'Quer ganhar prêmios em dinheiro? Com Unilever + PicPay dá! Vem saber mais.', date: 'Ontem às 19:05' },
+    { id: '4', content: 'Quer ganhar prêmios em dinheiro? Com Unilever + dá! Vem saber mais. Quer ganhar prêmios em dinheiro? Com Unilever + PicPay dá! Vem saber mais. Quer ganhar prêmios em dinheiro? Com Unilever + PicPay dá! Vem saber mais.', date: 'Ontem às 19:05' },
+    { id: '5', content: 'Quer ganhar prêmios em dinheiro? Com Unilever + PicPay dá! Vem saber mais.', date: 'Ontem às 19:05' },
+]
 export default () => {
-    return(
+
+    const ArrayNotification = ({ data }) => {
+        return (
+            <ArrayItem underlayColor="#ddd" onPress={() => console.log('Olá')}>
+                <>
+                    <DefaultText>{data.content}</DefaultText>
+                    <DefaultText mTop="10px" color="#aaa">{data.date}</DefaultText>
+                </>
+            </ArrayItem>
+        );
+    }
+    return (
         <Container>
-            <Texto>Olá mundo Notification</Texto>
+            <StatusBar barStyle="dark-content" backgroundColor="#eee" />
+
+            <Flat
+                ListHeaderComponent={
+                    <>
+                        <Header>
+                            <DefaultText bolder="bold" font="18px">Notificações</DefaultText>
+                            <BtnConfig onPress={() => console.log('Olá')}>
+                                <DefaultText bolder="bold" color="#36B477">Configurar</DefaultText>
+                            </BtnConfig>
+                        </Header>
+                    </>
+                }
+                data={array}
+                renderItem={({ item }) => <ArrayNotification data={item} />}
+                keyExtractor={(item) => item.id}
+            />
         </Container>
     );
 }
--- src/screens/NotificationScreen/styled.js
+++ src/screens/NotificationScreen/styled.js
@@ -1,5 +1,33 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+    background-color: #fff;
+    flex: 1;
+`;
 
-export const Texto = styled.Text``;
+export const Flat = styled.FlatList``;
+
+export const Header = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 15px;
+    background-color: #eee;
+    height: 57px;
+    borderBottomWidth: 1px;
+    borderBottomColor: #ddd;
+`;
+export const BtnConfig = styled.TouchableOpacity``;
+
+export const ArrayItem = styled.TouchableHighlight`
+    padding: 10px;
+    min-height: 90px;
+    borderBottomWidth: 1px;
+    borderBottomColor: #ddd;
+`;
+export const DefaultText = styled.Text`
+    color: ${props=>props.color || '#444'};
+    font-size: ${props=>props.font || '14px'};
+    margin-top: ${props=>props.mTop || '0'};
+    font-weight: ${props=>props.bolder || 'normal'};
+`;

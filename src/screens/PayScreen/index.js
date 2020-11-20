2ea914f464d0e62c03b7ae1307bbd8de9c781b39
Matheus Gomes <matheusgomes192@hotmail.com>
2020-11-20 14:28:08 -0300

    ADD svg de cobrança e finalizando PayScreen

--- src/screens/PayScreen/index.js
+++ src/screens/PayScreen/index.js
@@ -1,14 +1,89 @@
 import React from 'react';
+import Qr from '../../assets/svg/qr.svg';
+import UserCircle from '../../assets/svg/user_circle.svg';
+import BarScan from '../../assets/svg/bar_scan.svg';
+import ChargeMoney from '../../assets/svg/charge_money.svg';
+import Store from '../../assets/svg/store.svg';
+import Pin from '../../assets/svg/pin.svg';
+import Phone from '../../assets/svg/smartphone.svg';
+import Bus from '../../assets/svg/bus.svg';
 
 import {
     Container,
-    Texto
+
+    DefaultText,
+
+    Header,
+
+    MainContent,
+    Scroll,
+    RowItem,
+    SmallItem,
+
+    ColumnItem,
+    BigItem,
+    BigItemIcon,
+    BigItemContent,
 } from './styled';
 
+let bigItem = [
+    { id: '1', text: 'Places close to you', subText: 'Take a look at witch establishments are nearby', Icon: Pin },
+    { id: '2', text: 'Store', subText: 'Buy anything without leaving home', Icon: Store },
+];
+
+let smallItem = [
+    { id: '1', text: 'Pay bills', Icon: BarScan },
+    { id: '2', text: 'Pay someone', Icon: UserCircle },
+    { id: '3', text: 'Charge someone', Icon: ChargeMoney },
+    { id: '4', text: 'Smartphone recharge', Icon: Phone },
+    { id: '5', text: 'Bus pass', Icon: Bus, margin: false },
+];
+
+
+
 export default () => {
-    return(
+    return (
         <Container>
-            <Texto>Olá mundo Pay</Texto>
+            <Header>
+                <Qr width="25px" height="25px" fill="#1fa1c7" />
+                <DefaultText mLeft="15px" bolder="bold" font="20px">Transactions</DefaultText>
+            </Header>
+
+            <MainContent>
+
+                <ColumnItem>
+                    {bigItem.map((item, k) => (
+                        <BigItem key={k} onPress={() => console.log('olá mundo')} underlayColor="#ddd" >
+                            <>
+                                <BigItemIcon>
+                                    <item.Icon width="20px" height="20px" fill="#1fa1c7" />
+                                </BigItemIcon>
+                                <BigItemContent>
+                                    <DefaultText bolder="bold" font="16px">{item.text}</DefaultText>
+                                    <DefaultText>{item.subText}</DefaultText>
+                                </BigItemContent>
+                            </>
+                        </BigItem>
+                    ))}
+                </ColumnItem>
+
+                <Scroll decelerationRate="fast" showsHorizontalScrollIndicator={false} horizontal={true}>
+                    <RowItem>
+                        {smallItem.map((item, k) => (
+                            <SmallItem mRight={item.margin == false ? '0' : '10px'} key={k} onPress={() => console.log('olá mundo')} underlayColor="#ddd" >
+                                <>
+                                    <item.Icon width="20px" height="20px" fill="#1fa1c7" />
+                                    <DefaultText>{item.text}</DefaultText>
+                                </>
+                            </SmallItem>
+                        ))}
+                    </RowItem>
+                </Scroll>
+
+
+
+
+            </MainContent>
         </Container>
     );
 }
--- src/screens/PayScreen/styled.js
+++ src/screens/PayScreen/styled.js
@@ -1,5 +1,62 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: #fff;
+`;
 
-export const Texto = styled.Text``;
+export const DefaultText = styled.Text`
+    font-size: ${props=>props.font || '14px'};
+    font-weight: ${props=>props.bolder || 'normal'};
+    color: ${props=>props.color || '#000'};
+    margin-left: ${props=>props.mLeft || '0'};
+`;
+
+export const Header = styled.View`
+    flex-direction: row;
+    padding-left: 15px;
+    borderBottomWidth: 1px;
+    borderBottomColor: #ddd;
+    height: 55px;
+    align-items: center;
+`;
+
+export const MainContent = styled.View`
+    margin: 0 15px;
+`;
+export const Scroll = styled.ScrollView``;
+export const RowItem = styled.View`
+    flex-direction: row;
+    margin: 25px 0;
+`;
+export const SmallItem = styled.TouchableHighlight`
+    width: 110px;
+    height: 87px;
+    border-radius: 10px;
+    border: 1px solid #ddd;
+    justify-content: space-around;
+    padding: 0 10px;
+    margin-right: ${props=>props.mRight};
+`;
+
+
+export const ColumnItem = styled.View`
+    margin-top: 25px;
+`;
+export const BigItem = styled.TouchableHighlight`
+    height: 75px;
+    width: 100%;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    flex-direction: row;
+    margin-top: 10px;
+`;
+export const BigItemIcon = styled.View`
+    width: 50px;
+    align-items: center;
+    justify-content: center;
+`;
+export const BigItemContent = styled.View`
+    flex: 1;
+    justify-content: center;
+`;

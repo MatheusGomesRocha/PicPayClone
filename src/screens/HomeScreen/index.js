51d7d7f3ba773c60d4d3bda7944a6ac554eb6af8
Matheus Gomes <matheusgomes192@hotmail.com>
2020-11-22 13:33:45 -0300

    ADD bloco com TopTab para mostrar as atividades do usuário na HomeScreen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -3,6 +3,7 @@ import Qr from '../../assets/svg/qr.svg';
 import Gift from '../../assets/svg/gift.svg';
 import Cupom from '../../assets/svg/cupom.svg';
 import Search from '../../assets/svg/search.svg';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import { DefaultText } from '../../components/DefaultText';
 
@@ -18,12 +19,70 @@ import {
     RightHeader,
 
     BtnInput,
+
+    MainContent,
+    ActivityBlock,
+    ActivityTop,
+    ActivityIcon,
+    ActivityContent,
+    ActivityBottom,
 } from './styled';
 
+const TopTab = createMaterialTopTabNavigator();
+
+const array = [
+    { id: '1', type: 'Pagamento de Boleto', status: 'Realizado', price: '31,48', date: '20/11/2020' },
+    { id: '2', type: 'Pagamento de Boleto', status: 'Realizado', price: '31,48', date: '20/11/2020' },
+    { id: '3', type: 'Pagamento de Boleto', status: 'Realizado', price: '31,48', date: '20/11/2020' },
+    { id: '4', type: 'Pagamento de Boleto', status: 'Realizado', price: '31,48', date: '20/11/2020' },
+    { id: '5', type: 'Pagamento de Boleto', status: 'Realizado', price: '31,48', date: '20/11/2020' },
+]
+
 export default () => {
+    const History = () => {
+        return (
+            <MainContent>
+                {array.map((item, k) => (
+                    <ActivityBlock key={k}>
+                        <ActivityTop>
+
+                            <ActivityIcon>
+                                <Qr fill="#1fa1c7" width="20px" height="20px" />
+                            </ActivityIcon>
+                            <ActivityContent>
+                                <DefaultText font="16px">{item.type}</DefaultText>
+                                <DefaultText>{item.status}</DefaultText>
+                            </ActivityContent>
+
+                        </ActivityTop>
+
+                        <ActivityBottom>
+                            <DefaultText color="#1fa1c7">R$ {item.price}</DefaultText>
+                            <DefaultText mLeft="10px">{item.date}</DefaultText>
+                        </ActivityBottom>
+                    </ActivityBlock>
+                ))}
+
+            </MainContent>
+        );
+    }
+
+    const All = () => {
+        // CONTEÚDO DE TODOS, INCLUINDO PESSOAS QUE SIGO
+        return (
+            <History />
+        );
+    }
+
+    const My = () => {
+        // CONTEÚDO APENAS MEU
+        return (
+            <History />
+        );
+    }
     return (
         <Container>
-            <Scroll>
+            <Scroll showsVerticalScrollIndicator={false}>
                 <Header>
                     <LeftHeader>
                         <Qr width="25px" height="25px" fill="#1fa1c7" />
@@ -40,13 +99,28 @@ export default () => {
                     </RightHeader>
                 </Header>
 
-
                 <BtnInput underlayColor="transparent" onPress={() => console.log('Go to search')}>
                     <>
                         <Search style={{ marginRight: 10 }} width="25px" height="25px" fill="#1fa1c7" />
                         <DefaultText color="#aaa">What do you want to pay?</DefaultText>
                     </>
                 </BtnInput>
+
+                <TopTab.Navigator
+                    tabBarOptions={{
+                        style: { height: 60, marginLeft: 220, justifyContent: 'flex-end', elevation: 0, backgroundColor: 'transparent' },
+                        labelStyle: { fontSize: 16, textTransform: null },
+                        activeTintColor: '#1fa1c7',
+                        inactiveTintColor: '#ccc',
+                        tabStyle: { width: 80 },
+                        indicatorStyle: { backgroundColor: '#1fa1c7', height: 3 },
+                    }}
+                >
+                    <TopTab.Screen name="current" component={All} options={{ tabBarLabel: 'All' }} />
+                    <TopTab.Screen name="finish" component={My} options={{ tabBarLabel: 'My' }} />
+                </TopTab.Navigator>
+
+
             </Scroll>
         </Container>
     );
--- src/screens/HomeScreen/styled.js
+++ src/screens/HomeScreen/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
     flex: 1;
     padding: 0 15px;
+    background-color: #fff;
 `;
 
 export const Scroll = styled.ScrollView``;
@@ -33,5 +34,33 @@ export const BtnInput = styled.TouchableHighlight`
     border-radius: 25px;
     flex-direction: row;
     align-items: center;
+    padding-left: 20px;
+`;
+
+
+export const MainContent = styled.View`
+    background-color: #fff;
+`;
+export const ActivityBlock = styled.View`
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    height: 120px;
+    margin-top: 15px;
+    justify-content: space-around;
+`;
+export const ActivityTop = styled.View`
+    flex-direction: row;
+`;
+export const ActivityIcon = styled.View`
+    width: 50px;
+    align-items: center;
+    justify-content: center;
+`;
+export const ActivityContent = styled.View`
+    flex: 1;
+    justify-content: center;
+`;
+export const ActivityBottom = styled.View`
+    flex-direction: row;
     padding-left: 20px;
 `;

07e2d3f5b146ab6cdb75058ce7bf59078c8ee023
Matheus Gomes <matheusgomes192@hotmail.com>
2021-01-06 14:41:04 -0300

    Finalizando frontend da tela AddBankAccount

--- src/screens/AddBankAccountScreen/index.js
+++ src/screens/AddBankAccountScreen/index.js
@@ -1,39 +1,97 @@
-import React from 'react';
+import React, {useState, createRef} from 'react';
+import Teste from '../../assets/svg/search.svg';
+import Lock from '../../assets/svg/lock.svg';
 
 import {DefaultText} from '../../components/DefaultText';
 
 import {
     Container,
+    Scroll,
 
     Input,
     DivInputRow,
     DefaultView,
+    DefaultViewRow,
+    SquareBtn,
     DefaultBtn
 } from './styled';
 
 export default () => {
+    const [accountNumber, setAccountNumber] = useState();
+    const [agencyNumber, setAgencyNumber] = useState();
+    const [digit, setDigit] = useState();
+    const [type, setType] = useState();
+    const [bank, setBank] = useState();
+    const [ownerName, setOwnerName] = useState();
+    const [ownerCpf, setOwnerCpf] = useState();
+
+    const [notMyAccount, setNotMyAccount] = useState(false);
+
+    const agencyRef = createRef();
+    const digitRef = createRef();
+    const typeRef = createRef();
+    const bankRef = createRef();
+    const cpfRef = createRef();
+
     return (
         <Container>
-            <DefaultText mTop={"15px"} font={"16px"} color={"#aaa"}>To transfer money from Bankidô to your bank account, you need to
-                register a account firts</DefaultText>
+            <Scroll>
+                <DefaultText mTop={"15px"} font={"16px"} color={"#aaa"}>
+                    To transfer money from Bankidô to your bank account, you need to register a account first
+                </DefaultText>
 
-            <Input placeholder={"Account number"}/>
+                <Input onChangeText={(an => setAccountNumber(an))} returnKeyType={"next"}
+                       onSubmitEditing={() => agencyRef.current.focus()}
+                       placeholder={"Account number"}/>
 
-            <DivInputRow>
-                <Input width={"70%"} placeholder={"Agency number"}/>
-                <Input width={"25%"} placeholder={"Digit"}/>
-            </DivInputRow>
+                <DivInputRow>
+                    <Input onChangeText={(agn => setAgencyNumber(agn))} returnKeyType={"next"}
+                           onSubmitEditing={() => digitRef.current.focus()} ref={agencyRef}
+                           width={"70%"} placeholder={"Agency number"}/>
+                    <Input onChangeText={(d => setDigit(d))} returnKeyType={"next"}
+                           onSubmitEditing={() => typeRef.current.focus()} ref={digitRef}
+                           width={"25%"} placeholder={"Digit"}/>
+                </DivInputRow>
 
-            <Input placeholder={"Account type"}/>
-            <Input placeholder={"Bank"}/>
+                <Input onChangeText={(t => setType(t))} returnKeyType={"next"}
+                       onSubmitEditing={() => bankRef.current.focus()} ref={typeRef}
+                       placeholder={"Account type"}/>
+                <Input onChangeText={(b => setBank(b))} ref={bankRef} placeholder={"Bank"}/>
 
-            <DefaultView>
-                <DefaultText>This bank account isn't mine</DefaultText>
-            </DefaultView>
+                <DefaultView>
 
-            <DefaultBtn>
-                <DefaultText color={"#fff"} font={"16px"}>Save</DefaultText>
-            </DefaultBtn>
+                    <DefaultViewRow>
+                        <DefaultText>This bank account isn't mine</DefaultText>
+                        <SquareBtn onPress={() => setNotMyAccount(!notMyAccount)}>
+                            {notMyAccount &&
+                            <Teste width={20} height={20} fill={"#000"}/>
+                            }
+                        </SquareBtn>
+                    </DefaultViewRow>
+
+                    {notMyAccount &&
+                    <>
+                        <Input onChangeText={(on => setOwnerName(on))} returnKeyType={"next"}
+                               onSubmitEditing={() => cpfRef.current.focus()}
+                               placeholder={"Account Owner name"} mTop={"10px"}/>
+                        <Input onChangeText={(oc => setOwnerCpf(oc))} ref={cpfRef} placeholder={"Owner CPF"}
+                               mBottom={"10px"} mTop={"10px"}/>
+                    </>
+                    }
+                </DefaultView>
+
+                <DefaultBtn>
+                    <DefaultText color={"#fff"} font={"16px"}>Save</DefaultText>
+                </DefaultBtn>
+
+                <DefaultViewRow>
+                    <Lock width={50} height={50} fill={"#bbb"} />
+                    <DefaultText style={{width: '80%'}} mTop={"15px"} color={"#bbb"}>
+                        Bankidô it's 100% safe. this data will only be used when you do a transfer from Bankidô to other bank
+                    </DefaultText>
+                </DefaultViewRow>
+
+            </Scroll>
         </Container>
     );
 }
--- src/screens/AddBankAccountScreen/styled.js
+++ src/screens/AddBankAccountScreen/styled.js
@@ -5,12 +5,14 @@ export const Container = styled.SafeAreaView`
     background-color: #fff;
     padding: 0 5%;
 `;
+export const Scroll = styled.ScrollView``;
 
 export const Input = styled.TextInput`
     width: ${props=>props.width || '100%'};
     borderBottomColor: #000;
     borderBottomWidth: 1px;
-    margin-top: 20px;
+    margin-top: ${props=>props.mTop || '20px'};
+    margin-bottom: ${props=>props.mBottom || '0'};
     font-size: 16px;
 `;
 export const DivInputRow = styled.View`
@@ -19,14 +21,24 @@ export const DivInputRow = styled.View`
 `;
 export const DefaultView = styled.View`
     width: 100%;
-    height: 60px;
+    min-height: 60px;
     border: 1px solid #ccc;
     border-radius: 10px;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
     padding: 0 15px;
     margin-top: 25px;
+`;
+export const DefaultViewRow = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 15px;
+`;
+export const SquareBtn = styled.TouchableOpacity`
+    width: 30px;
+    height: 30px;
+    border: 2px solid #bbb;
+    justify-content: center;
+    align-items: center;
 `;
 export const DefaultBtn = styled.TouchableHighlight`
     width: 100%;
